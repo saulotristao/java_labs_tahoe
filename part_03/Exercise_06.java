af1b19d0a73d339f413b105f9c2683d5fabb47ec
Saulo Tristao <saulot@stanford.edu>
2018-06-14 10:28:11 -0700

    corrected number 6

--- part_03/Exercise_06.java
+++ part_03/Exercise_06.java
@@ -1,7 +1,5 @@
 package part_03;
 
-import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
-
 import java.util.Scanner;
 
 class Exercise_06 {
@@ -13,7 +11,6 @@ class Exercise_06 {
 
         System.out.print("Enter the time zone offset to GMT (ie, -8 or 8): ");
         long timeZoneChange = input.nextInt();
-        System.out.println(timeZoneChange);
 
         // Get total milliseconds since midnight, 1/1/1970
         long totalMilliseconds = System.currentTimeMillis();
@@ -22,33 +19,32 @@ class Exercise_06 {
         long totalSeconds = totalMilliseconds/1000;
 
         // Get the current second within the minute within the hour
-        long secondsPerDay = 3600 * 24;
-        long secondsPerHour = 3600;
         long secondsPerMinute = 60;
-
         long currentSecond = (totalSeconds % secondsPerMinute);
-        System.out.println(currentSecond);
+
         // Get total minutes
-        double currentMinutes = (totalSeconds % secondsPerHour);
+        long totalMinutes = totalSeconds/secondsPerMinute;
 
         // Get the current minute in the hour
-        long currentMinutes2 = (long)((currentMinutes/secondsPerHour) * secondsPerMinute);
-        System.out.println(currentMinutes2);
+        long minutesPerHour = 60;
+        long currentMinute = totalMinutes % minutesPerHour;
 
         // Get the total hours
-        double totalHours = (totalSeconds % secondsPerDay);
-        long totalHours1 = (long) (totalHours / secondsPerHour);
-        long totalHours2 = totalHours1 + timeZoneChange;
-        boolean flag = true; // true is AM
-        if (totalHours2 <0){
-            totalHours2+=12;
-            flag = false;
-        }
+        long secondsInHours = 3600;
+        long totalHours = totalSeconds /secondsInHours;
 
         // Get the current hour
+        long hoursPerDay = 24;
+        long currentHour = totalHours % hoursPerDay;
 
-        System.out.println(totalHours1);
-        System.out.println(totalHours2);
+        // the flag bellow is created to say if Am or Pm
+        boolean flag = true;
+        currentHour+= timeZoneChange;
+
+        if (currentHour > 12){
+            currentHour-=12;
+            flag = false;
+        }
 
         // Display results using a 12 hour clock, include AM or PM
         String amOrPm = "";
@@ -58,6 +54,6 @@ class Exercise_06 {
         else {
             amOrPm = "PM";
         }
-        System.out.println("it's  " + totalHours2 +":"+currentMinutes2+ ":"+currentSecond+ " "+ amOrPm);
+        System.out.println("it's  " + currentHour +":"+currentMinute+ ":"+currentSecond+ " "+ amOrPm);
     }
 }
