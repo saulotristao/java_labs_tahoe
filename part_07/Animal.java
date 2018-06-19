637f68f8e92c6b016ede850589310da7c9fea0bb
Saulo Tristao <saulot@stanford.edu>
2018-06-19 11:17:27 -0700

    part 07

---
+++ part_07/Animal.java
@@ -0,0 +1,12 @@
+package part_07;
+
+public interface Animal {
+
+    public boolean isAlive();
+
+    public String whereLives();
+
+    public boolean survivesInColdWeather(double currentTemperature);
+
+
+}
